d9b465514c3e61436cbee4eb04e54f54d0c60db6
adin <ratihadiningrum@gmail.com>
2023-03-24 22:48:36 +0700

    innitial commit

---
+++ src/notes.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+
+const notes = [
+    // {
+    //     method: 'POST',
+    //     path: '/notes',
+    //     handler: () => {},
+    // },
+];
+module.exports = notes;
---
+++ src/routes.js
@@ -0,0 +1,41 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
+const { 
+    addNoteHandler, 
+    getAllNotesHandler, 
+    getNoteByIdHandler, 
+    editNoteByIdHandler, 
+    deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+      method: 'POST',
+      path: '/notes',
+      handler: addNoteHandler,
+    },
+      {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+     },
+     {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByIdHandler,
+      },
+      {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+      },
+      {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+      },
+  ];
+
+module.exports = routes;
